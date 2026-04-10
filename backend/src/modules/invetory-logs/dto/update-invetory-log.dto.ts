9d8488c9f2bf9447571c7d0d9bc4eeeac3067837
Justik <chinhtc.it@gmail.com>
2026-04-10 10:01:59 +0700

    fix: detele module invetory-log because incorrect name

--- backend/src/modules/invetory-logs/dto/update-invetory-log.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInvetoryLogDto } from './create-invetory-log.dto';
-
-export class UpdateInvetoryLogDto extends PartialType(CreateInvetoryLogDto) {}
--- backend/src/modules/invetory-logs/invetory-logs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InvetoryLogsService } from './invetory-logs.service';
-import { InvetoryLogsController } from './invetory-logs.controller';
-
-@Module({
-  controllers: [InvetoryLogsController],
-  providers: [InvetoryLogsService],
-})
-export class InvetoryLogsModule {}
