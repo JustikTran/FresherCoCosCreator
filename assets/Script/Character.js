a606d112aa3206d37a2a5aae7780f7c5ec978ee6
Justik <chinhtc.it@gmail.com>
2026-04-16 15:27:48 +0700

    update UI

--- assets/Script/Character.js
+++ assets/Script/Character.js
@@ -2,13 +2,21 @@ cc.Class({
   extends: cc.Component,
 
   properties: {
-    currentX: 100,
+    moveDistance: 50,
     charName: "Demo player",
     mana: 100,
     processMana: cc.ProgressBar,
     labelName: cc.Label,
     anim: sp.Skeleton,
+    spine: cc.Node,
     isFaceRight: true,
+    speed: 10,
+  },
+
+  onLoad() {
+    this.isMoving = false;
+    this.targetX = 0;
+    this.currentX = this.node.x;
   },
 
   start() {
@@ -18,23 +26,49 @@ cc.Class({
   },
 
   update(dt) {
-    this.updateProcess();
     if (this.currentMana <= 0) {
       this.node.destroy();
     }
 
+    if (!this.isMoving) {
+      return;
+    }
+
+    this.updateProcess();
+
+    this.node.x += dt * this.step;
+
+    if (Math.abs(this.targetX - this.node.x) >= this.moveDistance) {
+      this.isMoving = false;
+      this.anim.animation = "idle";
+    }
   },
 
   updateProcess: function () {
     this.processMana.progress = this.currentMana / this.mana;
   },
 
-  move: function (event, param) {
-    this.currentMana -= 10;
-    if (+param > 0) {
-      this.node.x += 50;
-    } else {
-      this.node.x -= 50;
+  onMove: function (event, param = 1) {
+    if (this.isMoving) {
+      return;
     }
+    const dir = Number(param); 
+    this.targetX = this.node.x + this.moveDistance * dir;
+    if (this.targetX > 600 || this.targetX < -600) {
+      this.step = 0;
+      return;
+    }
+    this.anim.animation = "walk";
+
+    this.isMoving = true;
+    this.currentMana -= 5;
+
+    this.isFaceRight = dir === 1;
+
+    const absScaleX = Math.abs(this.spine.scaleX);
+    this.spine.scaleX = this.isFaceRight ? absScaleX : -absScaleX;
+
+
+    this.step = this.speed * dir;
   },
 });
