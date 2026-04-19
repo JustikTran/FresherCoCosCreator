df18b06065f3ac1dbce06995a065cd70d170e0d0
ChinhTC <assin2910@gmail.com>
2026-04-19 16:05:07 +0700

    add event emitter

---
+++ assets/Script/AnimationEvent.js
@@ -0,0 +1,19 @@
+const Emitter = require('./EventEmitter');
+cc.Class({
+    extends: cc.Component,
+
+    init(data) {
+        this.data = data;
+    },
+
+    start() {
+    },
+
+    onChangeSpine() {
+        Emitter.instance.emit("SPINE", this.data);
+    },
+
+    onChangeMode(event, mode) {
+        Emitter.instance.emit("MODE", mode);
+    }
+});
--- assets/Script/AnimationManager.js
+++ assets/Script/AnimationManager.js
@@ -1,14 +1,22 @@
+const Emitter = require('./EventEmitter');
 cc.Class({
   extends: cc.Component,
 
   properties: {
     prefab: cc.Prefab,
     spine: sp.Skeleton,
-    content: cc.Node
+    content: cc.Node,
+    animationLabel: cc.Label,
+    modeLabel: cc.Label,
   },
 
   onLoad() {
+    this.currentAnimation = null;
+    this.currentMode = null;
+
     this.content.children = [];
+    Emitter.instance.registerEvent("SPINE", this.setSpine.bind(this));
+    Emitter.instance.registerEvent("MODE", this.setMode.bind(this));
   },
 
   start() {
@@ -20,12 +28,57 @@ cc.Class({
     }
   },
 
+  onDestroy() {
+    Emitter.instance.removeEvent("SPINE", this.setSpine);
+    Emitter.instance.removeEvent("MODE", this.setMode);
+  },
+
+  setSpine(data) {
+    if (this.currentAnimation === data) {
+      this.currentAnimation = null;
+      this.playAnimation();
+      return;
+    }
+    this.currentAnimation = data;
+    this.playAnimation();
+  },
+
+  setMode(data) {
+    if (this.currentMode === data) {
+      this.currentMode = null;
+      this.playMode();
+      return;
+    }
+    this.currentMode = data;
+    this.playMode();
+  },
+
+  playAnimation() {
+    this.animationLabel.string = "Animation: " + (this.currentAnimation ?? 'None');
+
+    if (!this.currentAnimation) {
+      this.spine.animation = null;
+    } else {
+      this.spine.setAnimation(0, this.currentAnimation, true);
+    }
+  },
+
+  playMode() {
+    // if (!this.currentMode) {
+    //   return;
+    // }
+    this.modeLabel.string = "Mode: " + (this.currentMode || 'None');
+
+  },
+
   createButton(name) {
     let button = cc.instantiate(this.prefab);
     button
       .getChildByName("Background")
       .getChildByName("Label")
       .getComponent(cc.Label).string = name;
+
+    button.getComponent('AnimationEvent').init(name);
     return button;
   },
 });
