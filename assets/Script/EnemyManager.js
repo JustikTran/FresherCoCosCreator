be99256269896995ccaaca80ba2f9a2006bef243
Justik <chinhtc.it@gmail.com>
2026-04-20 17:33:46 +0700

    feat: add script enemy manager

---
+++ assets/Script/EnemyManager.js
@@ -0,0 +1,48 @@
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    dragonPrefab: {
+      type: cc.Prefab,
+      default: null,
+      serializable: true,
+    },
+    wolfPrefab: {
+      type: cc.Prefab,
+      default: null,
+      serializable: true,
+    },
+    dogPrefab: {
+      type: cc.Prefab,
+      default: null,
+      serializable: true,
+    },
+    secondSpawn: {
+      default: 2,
+      serializable: true,
+    },
+  },
+
+  // LIFE-CYCLE CALLBACKS:
+
+  onLoad () {
+    this.maxX = this.node.width;
+    this.maxY = this.node.height;
+    this.currentTime = 0;
+  },
+
+  start() {},
+
+  update (dt) {
+    if(this.currentTime >= this.secondSpawn){
+        this.currentTime = 0;
+        this.spawnEnemy();
+    }
+
+    this.currentTime += dt;
+  },
+
+  spawnEnemy(){
+    
+  }
+});
