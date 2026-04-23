49abfc98343450bce871300f0e2244066aecc37c
ChinhTC <assin2910@gmail.com>
2026-04-23 21:19:41 +0700

    add code base code

--- assets/script/manager/BulletManager.ts
+++ assets/script/manager/BulletManager.ts
@@ -1,14 +1,35 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab } from 'cc';
+import { mEmitter } from '../utils/EventBus';
+import { EventCall } from '../config/State';
 const { ccclass, property } = _decorator;
 
 @ccclass('BulletManager')
 export class BulletManager extends Component {
-    start() {
+    @property({ type: [Prefab] })
+    bulletPrefabs: Prefab[] = [];
 
+    onLoad(): void {
+        mEmitter.instance.registerEvent(
+            EventCall.SHOOT,
+            this.onShoot.bind(this),
+            this,
+        );
     }
 
-    update(deltaTime: number) {
-        
+    onShoot(vector2) {
+        let spawnPosition = this.node.convertToNodeSpaceAR(vector2);
+
+        this.spawnBullet(spawnPosition.x, spawnPosition.y);
+    }
+
+    spawnBullet(x, y) {
+        if (!this.currentBullet) {
+            return;
+        }
+
+        const bullet = instantiate(this.currentBullet);
+        bullet.getComponent("BulletController").init(x, y);
+        bullet.parent = this.node;
     }
 }
 
--- assets/script/manager/EnemyManager.ts
+++ assets/script/manager/EnemyManager.ts
@@ -1,8 +1,17 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('EnemyManager')
 export class EnemyManager extends Component {
+
+    @property({ type: [Prefab] })
+    enemyPrefabs: Prefab[] = [];
+
+    @property()
+    limitEnemy: number = 10;
+
+    
+    
     start() {
 
     }
