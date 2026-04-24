6cab963f7972349b89b25d78417eb1abd9fdd13c
Justik <chinhtc.it@gmail.com>
2026-04-24 09:18:44 +0700

    fix manager bullet

--- assets/script/controller/BulletController.ts
+++ assets/script/controller/BulletController.ts
@@ -1,8 +1,13 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('BulletController')
 export class BulletController extends Component {
+
+    init(position: Vec2){
+
+    }
+
     start() {
 
     }
--- assets/script/manager/BulletManager.ts
+++ assets/script/manager/BulletManager.ts
@@ -1,12 +1,15 @@
-import { _decorator, Component, Node, Prefab } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab, Vec2 } from 'cc';
 import { mEmitter } from '../utils/EventBus';
 import { EventCall } from '../config/State';
+import { BulletController } from '../controller/BulletController';
 const { ccclass, property } = _decorator;
 
 @ccclass('BulletManager')
 export class BulletManager extends Component {
     @property({ type: [Prefab] })
     bulletPrefabs: Prefab[] = [];
+
+    private _currentBullet: Node;
 
     onLoad(): void {
         mEmitter.instance.registerEvent(
@@ -17,18 +20,18 @@ export class BulletManager extends Component {
     }
 
     onShoot(vector2) {
-        let spawnPosition = this.node.convertToNodeSpaceAR(vector2);
+        let spawnPosition = this.node.position.set(vector2.x, vector2.y);
 
-        this.spawnBullet(spawnPosition.x, spawnPosition.y);
+        this.spawnBullet(spawnPosition);
     }
 
-    spawnBullet(x, y) {
-        if (!this.currentBullet) {
+    spawnBullet(position) {
+        if (!this._currentBullet) {
             return;
         }
 
-        const bullet = instantiate(this.currentBullet);
-        bullet.getComponent("BulletController").init(x, y);
+        const bullet = instantiate(this._currentBullet);
+        bullet.getComponent(BulletController).init(new Vec2(position));
         bullet.parent = this.node;
     }
 }
