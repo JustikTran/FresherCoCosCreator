ea7f760bde420c2c72415f6476bfe4715f987b10
ChinhTC <assin2910@gmail.com>
2026-04-27 15:14:46 +0700

    add game UI

--- assets/scripts/ui/Loading.ts
+++ assets/scripts/ui/Loading.ts
@@ -1,14 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, director, Label, Node, ProgressBar } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Loading')
 export class Loading extends Component {
-    start() {
+    @property(Label)
+    loadingLabel: Label = null;
+    @property(ProgressBar)
+    loadingBar: ProgressBar = null;
 
+    private _isLoaded: boolean = false;
+    private _sceneName: string = 'start';
+
+    start() {
+        director.preloadScene(
+            this._sceneName,
+            () => {
+                this._isLoaded = true;
+            }
+        )
     }
 
-    update(deltaTime: number) {
-        
+    update(dt: number): void {
+        if (this._isLoaded) {
+            director.loadScene(this._sceneName);
+        }
+    }
+
+    public setCallScene(sceneName: string) {
+        if (!sceneName) {
+            return;
+        }
+        this._sceneName = sceneName;
     }
 }
-
+
+
