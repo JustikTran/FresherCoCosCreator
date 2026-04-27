5283d050e18c3b052f0ae53b6880915c20344c76
ChinhTC <assin2910@gmail.com>
2026-04-27 11:47:19 +0700

    Init project

---
+++ assets/scripts/common/config.ts
@@ -0,0 +1,12 @@
+export enum EventType {
+    UPDATE_SCORE = "update_score",
+    GAME_OVER = "game_over",
+    GAME_START = "game_start"
+}
+
+export enum GameState {
+    LOADING,
+    START,
+    PLAYING,
+    GAME_OVER
+}
---
+++ assets/scripts/core/scene/BulletManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BulletManager')
+export class BulletManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
