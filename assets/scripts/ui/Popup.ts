e03769172c3034d7e0a112de95de141b1bfe09ec
Justik <chinhtc.it@gmail.com>
2026-04-28 14:50:55 +0700

    fix: fixgame UI

--- assets/scripts/ui/Popup.ts
+++ assets/scripts/ui/Popup.ts
@@ -1,14 +1,22 @@
 import { _decorator, Component, Node } from 'cc';
+import { GameManager } from '../core/global/GameManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('Popup')
 export class Popup extends Component {
-    start() {
 
+    public showSetting(): void {
+        GameManager.instance.showSettingPopup();
     }
 
-    update(deltaTime: number) {
-        
+    public showPause(): void {
+        GameManager.instance.showPausePopup();
+    }
+
+    public switchStart():void{
+        GameManager.instance.allPopupHide();
+        GameManager.instance.switchScene();
     }
 }
-
+
+
